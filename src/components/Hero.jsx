ce95bfc5c1b24541af28afd2650763c24a4f9a0f
James <jimibue@gmail.com>
2023-04-11 11:39:30 -0600

    init

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -38,7 +38,7 @@ export function Hero() {
   return (
     <div className="overflow-hidden bg-slate-900 dark:-mb-32 dark:mt-[-4.5rem] dark:pb-32 dark:pt-[4.5rem] dark:lg:mt-[-4.75rem] dark:lg:pt-[4.75rem]">
 
-      <div className="py-16 sm:px-2 lg:relative lg:py-20 lg:px-0 br"
+      <div style={{textAlign:'center'}} className="py-16 sm:px-2 lg:relative lg:py-20 lg:px-0 br"
     >
                     <p className="inline bg-gradient-to-r from-indigo-200 via-sky-400 to-indigo-200 bg-clip-text font-display text-7xl tracking-tight text-transparent">
                 The Joy of AI Painting.
@@ -49,14 +49,14 @@ export function Hero() {
        
            <div className="relative">
         
-              <p className="mt-3 text-2xl tracking-tight text-slate-400 bg-clip-text font-display text-transparent  ">
+              <p className="mt-3 text-2xl tracking-tight text-slate-400 bg-clip-text font-display text-transparent">
               Hello, friend! Join me, AI Bob Ross, as we create some happy little AI-generated paintings.
               </p>
               <div className="mt-8 flex gap-4 md:justify-center lg:justify-start">
-                <Button href="https://reactjs.org/">Midjourney</Button>
+                {/* <Button href="https://reactjs.org/">Midjourney</Button>
                 <Button href="https://github.com/facebook/react" variant="secondary">
                   Discord
-                </Button>
+                </Button> */}
               </div>
             </div>
           
